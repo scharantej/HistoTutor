d8ae78e5d24d9e2bbcd8b1d2a8c7a331324d5f59
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-08 16:07:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,71 +3,88 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
+# Set the secret key for session management
+app.secret_key = 'secret-key'
+
 @app.route('/')
-def home():
-  return render_template('index.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
 
 @app.route('/contact')
 def contact():
-  return render_template('contact.html')
+    return render_template('contact.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-  if request.method == 'POST':
-    username = request.form['username']
-    password = request.form['password']
-    if username == 'admin' and password == 'secret':
-      session['logged_in'] = True
-      return redirect(url_for('dashboard'))
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Validate the user credentials
+        if username == 'admin' and password == 'secret':
+            # Store the username in the session
+            session['username'] = username
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template('login.html', error='Invalid credentials')
     else:
-      return render_template('login.html', error='Invalid credentials')
-  else:
-    return render_template('login.html')
+        return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-  if request.method == 'POST':
-    username = request.form['username']
-    password = request.form['password']
-    if username == 'admin' and password == 'secret':
-      return render_template('register.html', error='Username already exists')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Validate the user input
+        if username and password:
+            # Create a new user account
+            # Store the user data in the database
+            return redirect(url_for('login'))
+        else:
+            return render_template('register.html', error='Please fill out all fields')
     else:
-      session['logged_in'] = True
-      return redirect(url_for('dashboard'))
-  else:
-    return render_template('register.html')
+        return render_template('register.html')
 
 @app.route('/dashboard')
 def dashboard():
-  if 'logged_in' in session:
-    return render_template('dashboard.html')
-  else:
-    return redirect(url_for('login'))
+    # Check if the user is logged in
+    if 'username' in session:
+        return render_template('dashboard.html')
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/lessons')
 def lessons():
-  if 'logged_in' in session:
-    return render_template('lessons.html')
-  else:
-    return redirect(url_for('login'))
+    # Check if the user is logged in
+    if 'username' in session:
+        return render_template('lessons.html')
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/quizzes')
 def quizzes():
-  if 'logged_in' in session:
-    return render_template('quizzes.html')
-  else:
-    return redirect(url_for('login'))
+    # Check if the user is logged in
+    if 'username' in session:
+        return render_template('quizzes.html')
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/resources')
 def resources():
-  if 'logged_in' in session:
-    return render_template('resources.html')
-  else:
-    return redirect(url_for('login'))
+    # Check if the user is logged in
+    if 'username' in session:
+        return render_template('resources.html')
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    # Clear the session and redirect to the home page
+    session.clear()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
